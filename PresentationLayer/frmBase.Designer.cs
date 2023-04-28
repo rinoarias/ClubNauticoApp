ef1f43c427312f77f6508450c940c062cd4bae3d
Rino Arias <rinoariaslaaz@gmail.com>
2023-04-28 18:02:17 -0500

    Agregar archivos de proyecto.

---
+++ PresentationLayer/frmBase.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace PresentationLayer
+{
+    partial class frmBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.cmbCriterioBusqueda = new System.Windows.Forms.ComboBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnNuevoRegistro = new System.Windows.Forms.Button();
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.lblTotalRegistros = new System.Windows.Forms.Label();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.lblNota = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(12, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(80, 29);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Titulo";
+            // 
+            // cmbCriterioBusqueda
+            // 
+            this.cmbCriterioBusqueda.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCriterioBusqueda.FormattingEnabled = true;
+            this.cmbCriterioBusqueda.Location = new System.Drawing.Point(12, 37);
+            this.cmbCriterioBusqueda.Name = "cmbCriterioBusqueda";
+            this.cmbCriterioBusqueda.Size = new System.Drawing.Size(121, 21);
+            this.cmbCriterioBusqueda.TabIndex = 1;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBuscar.Location = new System.Drawing.Point(142, 37);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(301, 20);
+            this.txtBuscar.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuscar.Location = new System.Drawing.Point(449, 36);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevoRegistro
+            // 
+            this.btnNuevoRegistro.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNuevoRegistro.Location = new System.Drawing.Point(530, 36);
+            this.btnNuevoRegistro.Name = "btnNuevoRegistro";
+            this.btnNuevoRegistro.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevoRegistro.TabIndex = 4;
+            this.btnNuevoRegistro.Text = "Nuevo Registro";
+            this.btnNuevoRegistro.UseVisualStyleBackColor = true;
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReporte.Location = new System.Drawing.Point(611, 36);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(75, 23);
+            this.btnReporte.TabIndex = 5;
+            this.btnReporte.Text = "Reporte";
+            this.btnReporte.UseVisualStyleBackColor = true;
+            // 
+            // lblTotalRegistros
+            // 
+            this.lblTotalRegistros.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalRegistros.AutoSize = true;
+            this.lblTotalRegistros.Location = new System.Drawing.Point(693, 41);
+            this.lblTotalRegistros.Name = "lblTotalRegistros";
+            this.lblTotalRegistros.Size = new System.Drawing.Size(85, 13);
+            this.lblTotalRegistros.TabIndex = 6;
+            this.lblTotalRegistros.Text = "Total registros: ?";
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.AllowUserToAddRows = false;
+            this.dgvDatos.AllowUserToDeleteRows = false;
+            this.dgvDatos.AllowUserToResizeRows = false;
+            this.dgvDatos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(12, 64);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(784, 361);
+            this.dgvDatos.TabIndex = 7;
+            // 
+            // lblNota
+            // 
+            this.lblNota.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblNota.AutoSize = true;
+            this.lblNota.Location = new System.Drawing.Point(12, 428);
+            this.lblNota.Name = "lblNota";
+            this.lblNota.Size = new System.Drawing.Size(385, 13);
+            this.lblNota.TabIndex = 8;
+            this.lblNota.Text = "* Nota: Haga doble clic en una fila para visualizar, modificar o eliminar el regi" +
+    "stro.";
+            // 
+            // frmBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNota);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.lblTotalRegistros);
+            this.Controls.Add(this.btnReporte);
+            this.Controls.Add(this.btnNuevoRegistro);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.cmbCriterioBusqueda);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "frmBase";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmBase";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ComboBox cmbCriterioBusqueda;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnNuevoRegistro;
+        private System.Windows.Forms.Button btnReporte;
+        private System.Windows.Forms.Label lblTotalRegistros;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.Label lblNota;
+    }
+}
---
+++ PresentationLayer/frmHomePage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class frmHomePage : Form
+    {
+        public frmHomePage()
+        {
+            InitializeComponent();
+            inicializarTableLayoutPanel();
+
+        }
+
+        private void inicializarTableLayoutPanel()
+        {
+            TableLayoutPanel tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.ColumnCount = 3;
+
+            // Establecer las dimensiones de las filas y columnas para que sean cuadradas
+            for (int i = 0; i < 3; i++)
+            {
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.33f));
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33f));
+            }
+
+            this.Controls.Add(tableLayoutPanel1);
+
+            string[] nombresBotones = { "Reporte", "Pais", "Ciudad", "Puerto", "Barco", "Persona", "Salida" };
+
+            for (int row = 0; row < Math.Ceiling((decimal)nombresBotones.Length / tableLayoutPanel1.ColumnCount); row++)
+            {
+                for (int col = 0; col < tableLayoutPanel1.ColumnCount && (row * tableLayoutPanel1.ColumnCount) + col < nombresBotones.Length; col++)
+                {
+                    Button button = new Button();
+                    button.Dock = DockStyle.Fill;
+                    button.AutoSize = false;
+                    button.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+                    button.Text = nombresBotones[(row * tableLayoutPanel1.ColumnCount) + col];
+                    button.Name = "btn" + button.Text;
+                    tableLayoutPanel1.Controls.Add(button, col, row);
+                    button.Click += new EventHandler(Button_Click);
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            //string nombreFormulario = "frm" + button.Text;
+            //Type tipoFormulario = Type.GetType(nombreFormulario);
+            //Form formulario = (Form)Activator.CreateInstance(tipoFormulario);
+            Form formulario = new frmBase();
+            formulario.ShowDialog();
+        }
+
+    }
+}
